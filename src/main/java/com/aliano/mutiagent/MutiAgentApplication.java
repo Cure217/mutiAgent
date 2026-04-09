16a9d7ec697fdec7266d298342eccc2ffa179eca
yucohu <yucohu@lexin.com>
2026-04-09 17:05:14 +0800

    Initial commit

---
+++ src/main/java/com/aliano/mutiagent/MutiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.aliano.mutiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aliano/mutiagent/MutiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliano.mutiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
